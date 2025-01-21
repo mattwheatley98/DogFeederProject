c0830990da29bca8344833235e4970ee698982d5
mattwheatley98 <113391095+mattwheatley98@users.noreply.github.com>
2025-01-21 10:18:08 -0500

    Update feedingTask.h

--- lib/src/feedingTask.h
+++ lib/src/feedingTask.h
@@ -2,10 +2,7 @@
 // Created by matt on 5/10/23.
 //
 
-#ifndef DOGFEEDERPROJECT_FEEDINGTASK_H
-#define DOGFEEDERPROJECT_FEEDINGTASK_H
-
-#endif //DOGFEEDERPROJECT_FEEDINGTASK_H
+#pragma once
 
 extern SemaphoreHandle_t timerFeedingSemaphore;
 extern QueueHandle_t displayCyclesQueue;
